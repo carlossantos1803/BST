6c73fc1e31d6020f54c4fe54ed0dab568a0b1766
carlos <carlossantos1803@gmail.com>
2020-08-24 14:50:10 -0400

    Pruebas
    Probando y mejorando el BST 2

--- Arbol Binario/Arbol Binario.cpp
+++ Arbol Binario/Arbol Binario.cpp
@@ -2,9 +2,88 @@
 //
 
 #include <iostream>
+class Node
+{
+public:
+	Node(const int& _key) : key(_key), left(nullptr), right(nullptr) {
+		printf("Construyendo Nodo\n");
+	};
+	~Node() {
+		printf("Destruyendo Nodo\n");
+		delete left;
+		delete right;
+	};
+	const int getKey() {
+		return key;
+	}
+	void setLeft(Node* _left) {
+		left = _left;
+	}
+	Node* getLeft() {
+		return left;
+	}
+	void setRight(Node* _right) {
+		right = _right;
+	}
+	Node* getRight() {
+		return right;
+	}
+private:
+	int key;
+	Node* left;
+	Node* right;
+};
+
+class BST
+{
+public:
+	BST() {};
+	~BST() {};
+	void add(const int& key) {
+		root = agregar(root, key);
+	}
+	
+	void show() {
+		mostrar(root);
+	}
+
+private:
+	Node* agregar(Node* x, const int& _key) {
+		if (x == nullptr) {
+			return new Node(_key);
+		}
+		else {
+			if (x->getKey() >= _key) {
+				x->setLeft(agregar(x->getLeft(), _key));
+			}
+			else if (x->getKey() < _key) {
+				x->setRight(agregar(x->getRight(), _key));
+			}
+		}
+		return x;
+	}
+	void mostrar(Node* x) {
+		if (x == nullptr) return;
+		//printf("%d ", x->getKey());
+		//if(x!=nullptr)
+		mostrar(x->getLeft());
+		printf("%d ", x->getKey());
+		//if (x != nullptr) 
+		mostrar(x->getRight());
+		//printf("%d ", x->getKey());
+	}
+	Node* root = nullptr;
+
+};
+
 
 int main()
 {
+	BST tree;
+	tree.add(25);
+	tree.add(15);
+	tree.add(35);
+	tree.show();
     std::cout << "Hello World!\n";
 }
 
